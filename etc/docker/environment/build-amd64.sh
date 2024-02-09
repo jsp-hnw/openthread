76fe68982b7644c558741d773390d4f27eb0c4a8
jsp <jsp@humax-networks.com>
2024-02-09 19:02:19 +0900

    non-root user 추가

--- etc/docker/environment/build-amd64.sh
+++ etc/docker/environment/build-amd64.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")"/../../../
 
 set -ex
-docker buildx build --platform linux/amd64 -t qorvo-ot-amd64 -f etc/docker/environment/Dockerfile .
+docker buildx build --platform linux/amd64 -t qorvo-ot-amd64 --build-arg USER_UID="$UID" -f etc/docker/environment/Dockerfile .
 exit 0
--- etc/docker/environment/build-arm64.sh
+++ etc/docker/environment/build-arm64.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")"/../../../
 
 set -ex
-docker buildx build --platform linux/arm64 -t qorvo-ot-arm64 -f etc/docker/environment/Dockerfile .
+docker buildx build --platform linux/arm64 -t qorvo-ot-arm64 --build-arg USER_UID="$UID" -f etc/docker/environment/Dockerfile .
 exit 0
